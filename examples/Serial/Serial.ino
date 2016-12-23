c6a5e2a3283f1b3b135bd51f1fd78d8b3b86778b
Kevin (Pololu) <dev-kevin@pololu.com>
2016-12-23 11:37:41 -0800

    fix wrong page number in comments

--- examples/Serial/Serial.ino
+++ examples/Serial/Serial.ino
@@ -8,7 +8,7 @@ particular device and full scale setting (gain).
 
 Example: An LSM6DS33 gives an accelerometer Z axis reading
 of 16276 with its default full scale setting of +/- 2 g. The
-LA_So specification in the LSM6DS33 datasheet (page 11)
+LA_So specification in the LSM6DS33 datasheet (page 15)
 states a conversion factor of 0.061 mg/LSB (least
 significant bit) at this FS setting, so the raw reading of
 16276 corresponds to 16276 * 0.061 = 992.8 mg = 0.9928 g.
