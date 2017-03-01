fc70d1f069a33c8704c10c088b64661bece3d71d
Aaron S. Crandall <acrandal@gmail.com>
2017-02-28 23:00:01 -0800

    Adding first example code for the new API updates.

---
+++ examples/SetFullScale/SetFullScale.ino
@@ -0,0 +1,51 @@
+/*
+ * The sensor outputs provided by the library are the raw
+ * 16-bit values obtained by concatenating the 8-bit high and
+ * low accelerometer and gyro data registers. They can be
+ * converted to units of g and dps (degrees per second) using
+ * the conversion factors specified in the datasheet for your
+ * particular device and full scale setting (gain).
+ * 
+ * Example: An LSM6DS33 gives an accelerometer Z axis reading
+ * of 16276 with its default full scale setting of +/- 2 g. The
+ * LA_So specification in the LSM6DS33 datasheet (page 15)
+ * states a conversion factor of 0.061 mg/LSB (least
+ * significant bit) at this FS setting, so the raw reading of
+ * 16276 corresponds to 16276 * 0.061 = 992.8 mg = 0.9928 g.
+ * 
+ * Original code from Pololu <inbox@pololu.com>
+ * Updates for Full Scale API by:
+ *  Aaron S. Crandall, 2017 - acrandal@gmail.com
+ */
+
+#include <Wire.h>
+#include <LSM6.h>
+
+LSM6 imu;
+
+char report[80];
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!imu.init())
+  {
+    Serial.println("Failed to detect and initialize IMU!");
+    while (1);
+  }
+  imu.enableDefault();
+}
+
+void loop()
+{
+  imu.read();
+
+  snprintf(report, sizeof(report), "A: %6d %6d %6d    G: %6d %6d %6d",
+    imu.a.x, imu.a.y, imu.a.z,
+    imu.g.x, imu.g.y, imu.g.z);
+  Serial.println(report);
+
+  delay(100);
+}
